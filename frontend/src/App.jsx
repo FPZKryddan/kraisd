7183bc0191ca1252f36f9168a3c0ebf66327ada5
Dennis Andersson <dennis.andersson106@gmail.com>
2024-12-11 15:21:35 +0100

    updated sliders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,11 +64,18 @@
                 <label htmlFor="negprompt" className='text-white'>Negative Prompt</label>
                 <textarea className="p-3 rounded-md" id='negprompt' type='text' rows="5" placeholder='Type negative prompt here...' value={negPrompt} onChange={(e) => setNegPrompt(e.target.value)}></textarea>
 
-                <label htmlFor='steps' className='text-white'>Steps: {steps}</label>
-                <input className="" type='range' min="1" max="200" step="1"id="steps" value={steps} onChange={(e) => setSteps(e.target.value)}></input>
+                <div className='flex flex-row gap-10'>
+                    <div className='flex flex-col basis-1/2'>
+                        <label htmlFor='steps' className='text-white'>Steps: {steps}</label>
+                        <input className="" type='range' min="1" max="200" step="1"id="steps" value={steps} onChange={(e) => setSteps(e.target.value)}></input>
 
-                <label htmlFor='cfg' className='text-white'>Guidance Scale: {cfg}</label>
-                <input className="" type='range' min="1" max="30" step="0.5" id="cfg" value={cfg} onChange={(e) => setCFG(e.target.value)}></input>
+                    </div>
+
+                    <div className='flex flex-col basis-1/2'>
+                        <label htmlFor='cfg' className='text-white'>Guidance Scale: {cfg}</label>
+                        <input className="" type='range' min="1" max="30" step="0.5" id="cfg" value={cfg} onChange={(e) => setCFG(e.target.value)}></input>
+                    </div>
+                </div>
 
                 <label htmlFor='seed' className='text-white'>Seed:</label>
                 <input className="p-2 rounded-md" type='number' max="10000000" id="seed" value={seed} onChange={(e) => setSeed(e.target.value)}></input>
@@ -81,7 +88,7 @@
                  onClick={submitPrompt}>Generate Image</button>
             </div>
             <div className="flex flex-col basis-3/5 h-full bg-transparent justify-center">
-                {image && <img className="self-center shadow-xl drop-shadow-2xl" src={image} width={1024} height={1024}/>}
+                {image && <img className="self-center shadow-xl drop-shadow-2xl hover:cursor-pointer hover:border-8 transition-all duration-75 ease-in-out" src={image} width={1024} height={1024}/>}
             </div>
             <div className="flex basis-1/5 h-full bg-transparent">
 
