6169079e026d93eabe47708bd6df2edb45dcf1c3
Dennis Andersson <dennis.andersson106@gmail.com>
2024-12-12 02:17:42 +0100

    implemented websockets and base queue view

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 import app.diffuser as sd
+from collections import deque
+import threading 
+import time
+
+
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+prompt_queue = deque()
 
 @app.route('/api', methods=["POST"])
 def index():
@@ -30,5 +40,51 @@ def index():
     image = sd.generate_image(prompt, negPrompt, steps, cfg, seed)
     return jsonify({"message": "image generated Successfully!", "image": image}), 200
 
+@socketio.on("connect")
+def connected():
+    print(request.sid)
+    print("client has connected")
+    emit("join",{"data":f"id: {request.sid} is connected"})
+
+@socketio.on("queue")
+def queue(prompt):
+    print(request.sid)
+    # TODO validate prompt data
+    task = {"sid": request.sid, "req": prompt}
+    prompt_queue.append(task)
+
+    update_client_queue(request.sid)
+
+
+def update_client_queue(sid):
+    client_queue = [{"id": idx, "item": item.get("req")} for idx, item in enumerate(prompt_queue) if item.get("sid") == sid]
+    socketio.emit("get_queue", {"queue": client_queue})
+
+def worker():
+    while True:
+        if prompt_queue:
+            task = prompt_queue.popleft()
+            client = task.get("sid")
+            task = task.get("req")
+            print(f"processing task: {task}")
+
+            prompt = task.get("prompt")
+            negPrompt = task.get("negPrompt")
+            steps = int(task.get("steps"))
+            cfg = float(task.get("cfg"))
+            seed = int(task.get("seed"))
+
+            try:
+                image = sd.generate_image(prompt, negPrompt, steps, cfg, seed)
+                socketio.emit("task_complete", {"image": image})
+                update_client_queue(client)
+            except Exception as e:
+                print(f"ERROR: {str(e)}")
+        else:
+            time.sleep(1)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    worker_thread = threading.Thread(target=worker, daemon=True)
+    worker_thread.start()
+    socketio.run(app, debug=True, port=5000)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,100 +1,147 @@
     import { useState, useEffect, useCallback } from 'react'
     import './App.css'
+    import {io} from 'socket.io-client'
 
     function App() {
-    const [image, setImage] = useState(null);
-    const [prompt, setPrompt] = useState(null);
-    const [negPrompt, setNegPrompt] = useState("");
-    const [steps, setSteps] = useState(20);
-    const [cfg, setCFG] = useState(7);
-    const [seed, setSeed] = useState(null);
-    const [randomize, setRandomize] = useState(true);
-    const api = import.meta.env.VITE_BACKEND_URL;
+        const [image, setImage] = useState(null);
+        const [prompt, setPrompt] = useState("");
+        const [negPrompt, setNegPrompt] = useState("");
+        const [steps, setSteps] = useState(20);
+        const [cfg, setCFG] = useState(7);
+        const [seed, setSeed] = useState(null);
+        const [randomize, setRandomize] = useState(true);
+        const [socket, setSocket] = useState(null);
+        const [queue, setQueue] = useState([]);
+        const api = import.meta.env.VITE_BACKEND_URL;
 
-    const decodeBase64Image = (base64Data, mimeType = "image/jpeg") => {
-        return `data:${mimeType};base64,${base64Data}`;
-    };
+        useEffect(() => {
+            if (!window.socket) {
+                window.socket = io("http://localhost:5000/", {
+                    transports: ["websocket"], // Ensure WebSocket transport is explicitly used
+                    cors: {
+                        origin: "*",           // Allow CORS
+                    },
+                });
 
-    const submitPrompt = useCallback(() => {
-        const opts = {
-            method: "POST",
-            headers: { 'Content-type': 'application/json' },
-            body: JSON.stringify({prompt: prompt, negPrompt: negPrompt, steps: steps, cfg: cfg, seed: seed})
+                
+                window.socket.on("join", (event) => {
+                    window.socket.send("Connection established")
+                    console.log("connected to server ", event.data)
+                });
+                
+                window.socket.on("leave", () => {
+                    console.log("Connection dropped")
+                });
+
+                window.socket.on("task_complete", (event) => {
+                    console.log(event)
+                    setImage(decodeBase64Image(event.image))
+                    console.log("recieved image!")
+                });
+
+                window.socket.on("get_queue", (event) => {
+                    console.log(event)
+                    setQueue(event.queue)
+                });
+
+                setSocket(window.socket);
+
+            }
+    
+
+        }, [])
+
+        const decodeBase64Image = (base64Data, mimeType = "image/jpeg") => {
+            return `data:${mimeType};base64,${base64Data}`;
+        };
+
+        const submitPrompt = useCallback(() => {
+            // const opts = {
+            //     method: "POST",
+            //     headers: { 'Content-type': 'application/json' },
+            //     body: JSON.stringify({prompt: prompt, negPrompt: negPrompt, steps: steps, cfg: cfg, seed: seed})
+            // }
+            // fetch(api + 'api', opts)
+            //     .then(response => response.json())
+            //     .then(data => setImage(decodeBase64Image(data.image)));
+            socket.emit("queue", {prompt: prompt, negPrompt: negPrompt, steps: steps, cfg: cfg, seed: seed})
+            if (randomize)
+                setSeed(Math.floor(Math.random() * 10000000))
+        }, [api, prompt, negPrompt, steps, cfg, seed, randomize])
+
+        const handleKeyPress = useCallback((e) => {
+            if (e.key == "Enter") {
+                submitPrompt()
+            }
+        }, [submitPrompt])
+
+        const toggleRandomize = () => {
+            setRandomize(!randomize)
         }
-        fetch(api + 'api', opts)
-            .then(response => response.json())
-            .then(data => setImage(decodeBase64Image(data.image)));
 
-        if (randomize)
-            setSeed(Math.floor(Math.random() * 10000000))
-    }, [api, prompt, negPrompt, steps, cfg, seed, randomize])
+        useEffect(() => {
+            window.addEventListener("keyup", handleKeyPress);
 
-    const handleKeyPress = useCallback((e) => {
-        if (e.key == "Enter") {
-            submitPrompt()
-        }
-    }, [submitPrompt])
+            return () => {
+                window.removeEventListener("keyup", handleKeyPress);
+            }
+        }, [handleKeyPress])
 
-    const toggleRandomize = () => {
-        setRandomize(!randomize)
-    }
+        useEffect(() => {
+            if (seed == null)
+                setSeed(Math.floor(Math.random() * 10000000))
+        }, [seed])
 
-    useEffect(() => {
-        window.addEventListener("keyup", handleKeyPress);
+        return (
+            <div className="container max-w-full m-0 p-5 h-screen bg-gradient-to-b from-black  to-blue-950 flex flex-row">
+                <div className="flex flex-col basis-1/5 h-full bg-slate-500 bg-opacity-30 rounded-lg border-white border-2
+                                p-5">
+                    <h1 className="text-white text-center text-3xl font-bold">Generation settings</h1>
 
-        return () => {
-            window.removeEventListener("keyup", handleKeyPress);
-        }
-    }, [handleKeyPress])
+                    <label htmlFor="prompt" className='text-white'>Prompt</label>
+                    <textarea className="p-3 rounded-md" id='prompt' type='text' rows="5" placeholder='Type prompt here...' value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
 
-    useEffect(() => {
-        if (seed == null)
-            setSeed(Math.floor(Math.random() * 10000000))
-    }, [seed])
+                    <label htmlFor="negprompt" className='text-white'>Negative Prompt</label>
+                    <textarea className="p-3 rounded-md" id='negprompt' type='text' rows="5" placeholder='Type negative prompt here...' value={negPrompt} onChange={(e) => setNegPrompt(e.target.value)}></textarea>
 
-    return (
-        <div className="container max-w-full m-0 p-5 h-screen bg-gradient-to-b from-black  to-blue-950 flex flex-row">
-            <div className="flex flex-col basis-1/5 h-full bg-slate-500 bg-opacity-30 rounded-lg border-white border-2
-                            p-5">
-                <h1 className="text-white text-center text-3xl font-bold">Generation settings</h1>
+                    <div className='flex flex-row gap-10'>
+                        <div className='flex flex-col basis-1/2'>
+                            <label htmlFor='steps' className='text-white'>Steps: {steps}</label>
+                            <input className="" type='range' min="1" max="200" step="1"id="steps" value={steps} onChange={(e) => setSteps(e.target.value)}></input>
 
-                <label htmlFor="prompt" className='text-white'>Prompt</label>
-                <textarea className="p-3 rounded-md" id='prompt' type='text' rows="5" placeholder='Type prompt here...' value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
+                        </div>
 
-                <label htmlFor="negprompt" className='text-white'>Negative Prompt</label>
-                <textarea className="p-3 rounded-md" id='negprompt' type='text' rows="5" placeholder='Type negative prompt here...' value={negPrompt} onChange={(e) => setNegPrompt(e.target.value)}></textarea>
-
-                <div className='flex flex-row gap-10'>
-                    <div className='flex flex-col basis-1/2'>
-                        <label htmlFor='steps' className='text-white'>Steps: {steps}</label>
-                        <input className="" type='range' min="1" max="200" step="1"id="steps" value={steps} onChange={(e) => setSteps(e.target.value)}></input>
-
+                        <div className='flex flex-col basis-1/2'>
+                            <label htmlFor='cfg' className='text-white'>Guidance Scale: {cfg}</label>
+                            <input className="" type='range' min="1" max="30" step="0.5" id="cfg" value={cfg} onChange={(e) => setCFG(e.target.value)}></input>
+                        </div>
                     </div>
 
-                    <div className='flex flex-col basis-1/2'>
-                        <label htmlFor='cfg' className='text-white'>Guidance Scale: {cfg}</label>
-                        <input className="" type='range' min="1" max="30" step="0.5" id="cfg" value={cfg} onChange={(e) => setCFG(e.target.value)}></input>
-                    </div>
+                    <label htmlFor='seed' className='text-white'>Seed:</label>
+                    <input className="p-2 rounded-md" type='number' max="10000000" id="seed" value={seed} onChange={(e) => setSeed(e.target.value)}></input>
+                    <label htmlFor='randomizeCheck' className='text-white'>Randomize seed?</label>
+                    <input id="randomizeCheck" type="checkbox" checked={randomize} onClick={toggleRandomize}></input>
+
+                    <button className="bg-blue-600 rounded-md border-2 border-black self-center m-5 p-2
+                                        hover:bg-blue-900 text-white text-2xl shadow-lg hover:scale-95 hover:rounded-full 
+                                        transition-all duration-100 ease-in-out"
+                    onClick={submitPrompt}>Generate Image</button>
                 </div>
+                <div className="flex flex-col basis-3/5 h-full bg-transparent justify-center">
+                    {image && <img className="self-center shadow-xl drop-shadow-2xl hover:cursor-pointer hover:border-8 transition-all duration-75 ease-in-out" src={image} width={1024} height={1024}/>}
+                </div>
+                <div className="flex flex-col basis-1/5 h-full p-5 bg-slate-500 bg-opacity-30 rounded-lg border-white border-2">
+                    <h2 className='text-center text-2xl text-white border-b-2' >Queue</h2>
+                    <ul className=''>
+                        {queue.map((item) => (
+                            <li key={item.id}
+                                className='text-white text-xl'>{item.id}</li>
+                        ))}
+                    </ul>
 
-                <label htmlFor='seed' className='text-white'>Seed:</label>
-                <input className="p-2 rounded-md" type='number' max="10000000" id="seed" value={seed} onChange={(e) => setSeed(e.target.value)}></input>
-                <label htmlFor='randomizeCheck' className='text-white'>Randomize seed?</label>
-                <input id="randomizeCheck" type="checkbox" checked={randomize} onClick={toggleRandomize}></input>
-
-                <button className="bg-blue-600 rounded-md border-2 border-black self-center m-5 p-2
-                                    hover:bg-blue-900 text-white text-2xl shadow-lg hover:scale-95 hover:rounded-full 
-                                    transition-all duration-100 ease-in-out"
-                 onClick={submitPrompt}>Generate Image</button>
+                </div>
             </div>
-            <div className="flex flex-col basis-3/5 h-full bg-transparent justify-center">
-                {image && <img className="self-center shadow-xl drop-shadow-2xl hover:cursor-pointer hover:border-8 transition-all duration-75 ease-in-out" src={image} width={1024} height={1024}/>}
-            </div>
-            <div className="flex basis-1/5 h-full bg-transparent">
-
-            </div>
-        </div>
-    )
+        )
     }
 
     export default App
