b18cdeaf41eb26844ca66e59ef139b674e09abef
Dennis Andersson <dennis.andersson106@gmail.com>
2024-12-13 01:27:12 +0100

    added colors to tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -110,44 +110,52 @@
         }, [seed])
 
         return (
-            <div className="container max-w-full m-0 p-5 h-screen bg-gradient-to-b from-black  to-blue-950 flex flex-row">
-                <div className="flex flex-col basis-1/5 h-full bg-slate-500 bg-opacity-30 rounded-lg border-white border-2
+            <div className="container max-w-full m-0 p-5 h-screen 
+                            bg-primary-indigo
+                            flex flex-row justify-center gap-[20px]">
+                <div className="flex flex-col basis-[60%] gap-5">
+                    <h1 className='text-6xl text-text-primary text-center font-extrabold mt-[70px] relative'>KRAISD</h1>
+                    <textarea className="p-3 rounded-md w-2/3 self-center text-text-black placeholder:text-text-muted border-neutral-muted border-2" id='prompt' type='text' rows="1" placeholder='Type prompt here...' value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
+                </div>
+                {/* <div className="flex flex-col basis-1/5 h-full 
+                                bg-secondary-coral
+                                rounded-lg border-neutral-muted border-2
                                 p-5 gap-2">
-                    <h1 className="text-white text-center text-3xl font-bold">Generation settings</h1>
+                    <h1 className="text-text-primary text-center text-3xl font-bold">Generation settings</h1>
 
-                    <label htmlFor="prompt" className='text-white text-xl'>Prompt</label>
+                    <label htmlFor="prompt" className='text-text-primary text-xl'>Prompt</label>
                     <textarea className="p-3 rounded-md" id='prompt' type='text' rows="5" placeholder='Type prompt here...' value={prompt} onChange={(e) => setPrompt(e.target.value)}></textarea>
 
-                    <label htmlFor="negprompt" className='text-white text-xl'>Negative Prompt</label>
+                    <label htmlFor="negprompt" className='text-text-primary text-xl'>Negative Prompt</label>
                     <textarea className="p-3 rounded-md" id='negprompt' type='text' rows="5" placeholder='Type negative prompt here...' value={negPrompt} onChange={(e) => setNegPrompt(e.target.value)}></textarea>
 
                     <div className='flex flex-row gap-10'>
                         <div className='flex flex-col basis-1/2'>
-                            <label htmlFor='steps' className='text-white text-xl'>Steps: {steps}</label>
+                            <label htmlFor='steps' className='text-text-primary text-xl'>Steps: {steps}</label>
                             <input className="" type='range' min="1" max="200" step="1"id="steps" value={steps} onChange={(e) => setSteps(e.target.value)}></input>
 
                         </div>
 
                         <div className='flex flex-col basis-1/2'>
-                            <label htmlFor='cfg' className='text-white text-xl'>Guidance Scale: {cfg}</label>
+                            <label htmlFor='cfg' className='text-text-primary text-xl'>Guidance Scale: {cfg}</label>
                             <input className="" type='range' min="1" max="30" step="0.5" id="cfg" value={cfg} onChange={(e) => setCFG(e.target.value)}></input>
                         </div>
                     </div>
 
-                    <label htmlFor='seed' className='text-white text-xl'>Seed:</label>
+                    <label htmlFor='seed' className='text-text-primary text-xl'>Seed:</label>
                     <input className="p-2 rounded-md" type='number' max="10000000" id="seed" value={seed} disabled={randomize} onChange={(e) => setSeed(e.target.value)}></input>
                     <div className='flex flex-row gap-2 text-xl'>
-                        <label htmlFor='randomizeCheck' className='text-white'>Randomize seed?</label>
+                        <label htmlFor='randomizeCheck' className='text-text-primary'>Randomize seed?</label>
                         <input id="randomizeCheck" className="size-6 self-end" type="checkbox" checked={randomize} onClick={toggleRandomize}></input>
                     </div>
 
-                    <button className="bg-blue-500 rounded-md self-center m-5 p-2
-                                        hover:bg-blue-900 text-white text-2xl shadow-lg hover:shadow-2xl hover:drop-shadow-2xl
+                    <button className="bg-accent-green rounded-md self-center m-5 p-2
+                                        hover:bg-blue-900 text-text-primary text-2xl shadow-lg hover:shadow-2xl hover:drop-shadow-2xl
                                         transition-all duration-100 ease-in-out"
                     onClick={submitPrompt}>Generate Image</button>
                 </div>
                 <div className="flex flex-col basis-3/5 h-full bg-transparent items-center">
-                    <div className={'w-3/4 text-white text-lg ' + (generating == true ? "visible" : "invisible")}>
+                    <div className={'w-3/4 text-text-primary text-lg ' + (generating == true ? "visible" : "invisible")}>
                         <label htmlFor="generationProgress">Generation steps: {generatedSteps} / {totalGenerationSteps}</label>
                         <progress id="generationProgress" className="w-full" value={generatedSteps} max={totalGenerationSteps}></progress>
 
@@ -155,16 +163,16 @@
                     {image && <img className="self-center shadow-xl drop-shadow-2xl hover:cursor-pointer hover:border-8 transition-all duration-75 ease-in-out" src={image} style={{ width: "auto", height: "auto" }}/>}
                 </div>
                 <div className="flex flex-col basis-1/5 h-full p-5 bg-slate-500 bg-opacity-30 rounded-lg border-white border-2">
-                    <h2 className='text-center text-2xl text-white border-b-2' >Queue</h2>
+                    <h2 className='text-center text-2xl text-text-primary border-b-2' >Queue</h2>
                     <ul className=''>
                         {queue.map((item) => (
                             <li key={item.id}
-                                className='text-white text-xl'>{item.id}</li>
+                                className='text-text-primary text-xl'>{item.id}</li>
                         ))}
                     </ul>
 
-                </div>
-            </div>
+                </div>*/}
+            </div> 
         )
     }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        indigo: '#202040',
+        purple: '#543864'
+      },
+      secondary: {
+        coral: '#FF6363',
+        mustard: '#FFBD69'
+      },
+      accent: {
+        green: '#10B981',
+        blue: '#4F46E5'
+      },
+      neutral: {
+        light: '#F5f5f5',
+        soft: '#E5E7EB',
+        muted: '#D1D5DB'
+      },
+      text: {
+        primary: '#FFFFFF',
+        secondary: '#E5E5E5',
+        muted: '#A0A3B1',
+        black: '#000'
+      }
+
+    },
   },
   plugins: [],
 }
